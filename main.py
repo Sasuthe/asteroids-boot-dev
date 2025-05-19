306f930278a5d86b79177eddc9ad0449e5ff8056
sasuthe <truetexs1989@gmail.com>
2025-05-19 15:51:54 -0500

    B: Updated game loop

--- main.py
+++ main.py
@@ -3,20 +3,23 @@ from constants import *
 
 def main():
     pygame.init()
-
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
 
     while True:
+        # Event to allow exiting game
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
-        pygame.Surface.fill(screen, (0,0,0))
+
+        # Full screen black with refresh
+        screen.fill((0,0,0))
         pygame.display.flip()
+
+        # Limit Framerate to 60
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
