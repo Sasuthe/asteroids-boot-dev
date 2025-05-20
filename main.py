5ccc1845967ce2e814cd684c589ca9b4b1484564
sasuthe <truetexs1989@gmail.com>
2025-05-20 15:26:49 -0500

    D: Added player rotation

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
+    print("Starting game...")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
 
     while True:
         # Event to allow exiting game
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
+        
         # Full screen black with refresh
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
-
+        
+        player.update(dt)
         # Limit Framerate to 60
         dt = clock.tick(60) / 1000
 
---
+++ player.py
@@ -0,0 +1,34 @@
+import pygame
+from circleshape import *
+from constants import *
+
+class Player(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+
+    def triangle(self):
+        # Unit vectors
+        forward = pygame.Vector2(0, -1).rotate(self.rotation)  # -1 to make "0" point up
+        right = forward.rotate(90) * self.radius / 1.5
+
+        tip = self.position + forward * self.radius
+        left = self.position - forward * self.radius - right
+        right_corner = self.position - forward * self.radius + right
+
+        return [tip, left, right_corner]
+
+    def draw(self, screen):
+        pygame.draw.polygon(screen, "white", [v.xy for v in self.triangle()], width=2)
+
+    def rotate(self, dt):
+        self.rotation += (PLAYER_TURN_SPEED * dt)
+    
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+
+        if keys[pygame.K_d]:
+            self.rotate(dt)
